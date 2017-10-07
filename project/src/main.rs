1cfd85d8b9fa6b9ee19a6c9488f04371c8d90c8d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-07 22:32:13 +0200

    Configure a logger

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use] extern crate log;
+extern crate simplelog;
 extern crate iron;
 extern crate staticfile;
 extern crate mount;
@@ -7,8 +9,17 @@ use std::path::Path;
 use iron::{Iron, Chain};
 use staticfile::Static;
 use mount::Mount;
+use simplelog::{Config, LogLevelFilter, TermLogger, CombinedLogger};
 
 fn main() {
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LogLevelFilter::Info, Config::default()).unwrap(),
+        ]
+    ).unwrap();
+
+    info!("Logger configured");
+
     let mut mount = Mount::new();
 
     mount.mount("/", Static::new(Path::new("client/")));
