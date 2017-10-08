b6b17ac1e83a1d4fb8cba0671ec2a47fc28e6588
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-08 13:03:12 +0200

    Create an example event and serialize it

--- project/src/api/mod.rs
+++ project/src/api/mod.rs
@@ -1,3 +1,4 @@
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Event {
     origin: String,
     message: String,
@@ -9,5 +10,21 @@ impl Event {
             origin: origin.to_string(),
             message: message.to_string(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn should_be_able_to_serialize_to_json() {
+        let original = Event::new("test", "Hello, World!");
+
+        let serialized = serde_json::to_string(&original).unwrap();
+        let deserialized = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(original, deserialized);
     }
 }
