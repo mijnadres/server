54a1d31e998c733b5f7d7e5f6fdeb4bf90657454
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-12 08:58:09 +0200

    Create a database connection pool

--- project/src/bin/server.rs
+++ project/src/bin/server.rs
@@ -8,8 +8,13 @@ extern crate iron;
 extern crate staticfile;
 extern crate mount;
 extern crate logger;
+extern crate dotenv;
+extern crate r2d2;
+extern crate r2d2_diesel;
 extern crate mijnadres_server as adr;
 
+use std::env;
+use dotenv::dotenv;
 use std::path::Path;
 
 use iron::{Iron, Chain};
@@ -17,8 +22,11 @@ use staticfile::Static;
 use mount::Mount;
 use simplelog::{Config, LogLevelFilter, TermLogger, CombinedLogger};
 use logger::Logger;
+use diesel::pg::PgConnection;
+use r2d2_diesel::ConnectionManager;
 
 fn main() {
+    dotenv().ok();
     CombinedLogger::init(
         vec![
             TermLogger::new(LogLevelFilter::Info, Config::default()).unwrap(),
@@ -26,6 +34,13 @@ fn main() {
     ).unwrap();
 
     info!("Logger configured");
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    let config = r2d2::Config::default();
+    let manager = r2d2_diesel::ConnectionManager::new(database_url);
+    let pool: r2d2::Pool<ConnectionManager<PgConnection>> = r2d2::Pool::new(config, manager)
+        .expect("failed to create database pool");
 
     Iron::new(chain()).http("localhost:3000").unwrap();
 }
