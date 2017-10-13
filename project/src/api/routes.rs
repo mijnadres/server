6e34e1916040b9f026c01bfce6820dc4008c77f6
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-13 09:59:37 +0200

    Return an event from the database

--- project/src/api/routes.rs
+++ project/src/api/routes.rs
@@ -1,7 +1,10 @@
-use iron::{Request, Response, IronResult, IronError, status};
+use iron::{Request, Response, IronResult, IronError, Plugin, status};
 use iron::headers::ContentType;
 use router::Router;
+use persistent::Read;
 use serde_json;
+use diesel::prelude::*;
+use super::super::database::ConnectionPool;
 
 use super::Event;
 
@@ -12,12 +15,23 @@ pub fn router() -> Router {
     router
 }
 
-fn handler(_: &mut Request) -> IronResult<Response> {
-    let event = Event::new("mijnadres", "Hello, World");
+fn handler(request: &mut Request) -> IronResult<Response> {
+    use super::super::schema::events::dsl::*;
+
+    let arc = request.get::<Read<ConnectionPool>>().unwrap();
+    let connection = arc.get().unwrap();
+
+    let past_events: Vec<Event> = events
+        .limit(5)
+        .load::<Event>(&*connection)
+        .expect("Error loading past events");
+
+    let default_event = Event::new("default", "default");
+    let event = past_events.first().unwrap_or(&default_event);
 
     match serde_json::to_string(&event) {
-        Ok(message) => {
-            let mut response = Response::with((status::Ok, message));
+        Ok(m) => {
+            let mut response = Response::with((status::Ok, m));
             response.headers.set(ContentType::json());
             Ok(response)
         },
